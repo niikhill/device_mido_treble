57088e8d68f46bcf530b9ac36713954cf3c1ed50
Nikhil <5293647+niikhill@users.noreply.github.com>
2018-05-28 16:04:21 +0000

    Pe Bringup

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -17,9 +17,12 @@
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_mido
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aosp_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
